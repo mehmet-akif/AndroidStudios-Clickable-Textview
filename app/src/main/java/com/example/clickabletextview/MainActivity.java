660fc1a117a94f283aa919af9aa820e22d11a9e7
mehmet-akif <makif8992@gmail.com>
2023-12-19 11:01:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clickabletextview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.clickabletextview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text_view);
+        String text = "For more information, click here";
+        SpannableString ss = new SpannableString(text);
+
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+
+                Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT);
+
+
+            }
+        };
+
+        ss.setSpan(clickableSpan1, 22,32, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
